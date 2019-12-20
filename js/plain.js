499f78b8a5991eb9ad3d8802464f7d9b20662b5c
Brynjar Harðarson <brynjar@gmail.com>
2019-12-20 15:41:31 +0000

    testing CORS proxy

--- js/plain.js
+++ js/plain.js
@@ -46,12 +46,18 @@ function addImage(data) {
 function toDataUrl(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
+        console.log("onload...")
         var reader = new FileReader();
         reader.onloadend = function() {
             callback(reader.result);
         }
         reader.readAsDataURL(xhr.response);
     };
+    xhr.onerror = function(error) {
+        this.onerror = null;
+        this.open('GET', "https://cors.io/?"+url);
+        this.send();
+    }
     xhr.open('GET', url);
     xhr.responseType = 'blob';
     xhr.send();
